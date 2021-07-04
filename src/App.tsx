1c48537d514c3cf0e254291a930700ea9ca66b45
GabrielVasconcelosMarques <gabrielvasco3431@gmail.com>
2021-07-04 13:35:16 -0300

    Terceiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createContext,useState, useEffect } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { auth, firebase } from './services/firebase';
 
@@ -8,15 +8,19 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 import { AuthContextProvider } from './contexts/AuthContext';
+import { Room } from './pages/Room';
 
 
-
+// O switch faz com que retorne apenas uma rota satisfeita
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch> 
+          <Route path="/" exact component={Home} /> 
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
         </AuthContextProvider>
     </BrowserRouter>
   );
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
+import { database } from '../services/firebase';
 
 import '../styles/auth.scss';
 
@@ -9,7 +11,31 @@ import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom(){
-    //const { user } = useAuth()
+    const { user } = useAuth();
+    const history = useHistory();
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        // função para previnir que a página fique atualizando após clicar no submit do formulário
+        event.preventDefault();
+        
+        if(newRoom.trim() === ''){
+            return 
+        }
+
+        // criando no bd o rooms e dentro dele jogando os dados de quem criou a sala, como id e nome da sala
+        // procurar por referencia rooms dentro do banco de dados e depois fazer um push
+        const roomRef = database.ref('rooms');
+
+        // jogando umam informação pra dentro de rooms, jogando uma nova sala pra dentro de rooms
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+        });
+
+        history.push(`/rooms/${firebaseRoom.key}`);
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -21,8 +47,8 @@ export function NewRoom(){
                 <div className="main-content">
                     <img src={logoImg} alt="LetMeAsk" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input type="text" name="" id="" placeholder="Nome da sala"/>
+                    <form onSubmit={handleCreateRoom}>
+                        <input onChange={event => setNewRoom(event.target.value)} value={newRoom} type="text" name="" id="" placeholder="Nome da sala"/>
                         <Button type="submit">
                             Criar sala
                         </Button>
